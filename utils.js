c5042eb9be41960ee27f5fa920a101730cd5d580
Dani Vicario <danigv.email@gmail.com>
2019-03-19 11:45:24 +0100

    function hoisting example with anonymous functions

---
+++ utils.js
@@ -0,0 +1,12 @@
+const danimaths = {
+    randomIntFromInterval(min, max) // min and max included
+    {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    },
+    calculateRandomColor() {
+
+    }
+}
+
+utils.randomIntFromInterval(10, 100)
+utils.calculateRandomColor()
